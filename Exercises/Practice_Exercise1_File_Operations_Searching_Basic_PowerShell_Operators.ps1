216df2105a84ce4d1a1471ac8c02a8cf9c24ffbc
Y <gfranco@hotmail.co.uk>
2022-06-22 16:03:56 +0100

    asfa

---
+++ Exercises/Practice_Exercise1_File_Operations_Searching_Basic_PowerShell_Operators.ps1
@@ -0,0 +1,51 @@
+New-Item "C:\TestingPurpose" -ItemType Directory
+# mkdir $(1..3 | %{"SubFolder$_"})
+1..3 | foreach {mkdir C:\TestingPurpose\SubFolder$_}
+1..50 | foreach {new-item -path C:\TestingPurpose\SubFolder1\TypeATest$_.txt}
+51..100 | foreach {new-item -path C:\TestingPurpose\SubFolder2\TypeBTest$_.txt}
+101..150 | foreach {new-item -path C:\TestingPurpose\SubFolder3\TypeCTest$_.txt}
+101..150 | foreach {remove-item -path C:\TestingPurpose\SubFolder1\TypeCTest$_.txt}
+
+# $srcpath = "C:\TestingPurpose\SubFolder3"
+# $dstpath = "C:\TestingPurpose\SubFolder2"
+# Get-ChildItem -File -Recurse -Path $srcpath |
+#     ForEach-Object {
+#         if($_.Name -like '*1.txt', '*3.txt', '*5.txt', '*7.txt', '*9.txt') {
+#             Move-Item -Path $_.FullName -Destination $dstpath
+#         }
+#     }
+# $srcpath = "C:\TestingPurpose\SubFolder3"
+# $dstpath = "C:\TestingPurpose\SubFolder2"
+# $files = $srcpath
+# foreach ($file in $files) {
+#     if ($file -like '*1.txt', '*3.txt', '*5.txt', '*7.txt', '*9.txt') {
+#         Move-Item -Destination $dstpath
+#     }
+# }
+# Finding odd digits and moving them
+$srcpath = "C:\TestingPurpose"
+$dstpath = "C:\TestingPurpose\SubFolder2"
+Get-ChildItem -File -Recurse -Path $srcpath |
+    ForEach-Object {
+        # look for a digit
+        if($_.BaseName -match '\d$') {
+            # is that digit odd?
+            if([int]($Matches[0]) % 2 -eq 1) {
+                Move-Item -Path $_.FullName -Destination $dstpath
+            }
+        }
+    }
+
+#Finding even digits and moving them 
+$srcpath = "C:\TestingPurpose"
+$dstpath = "C:\TestingPurpose\SubFolder1"
+Get-ChildItem -File -Recurse -Path $srcpath |
+    ForEach-Object {
+        # look for a digit
+        if($_.BaseName -match '\d$') {
+            # is that digit even?
+            if([int]($Matches[0]) % 2 -eq 0) {
+                Move-Item -Path $_.FullName -Destination $dstpath
+            }
+        }
+    }
