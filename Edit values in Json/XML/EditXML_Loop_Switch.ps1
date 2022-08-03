0e85aede9e75034884cc1c6776102b24e4882b09
Y <gfranco@hotmail.co.uk>
2022-08-03 16:04:59 +0100

    PS script that loops through and changes multiple node values if prompted

---
+++ Edit values in Json/XML/EditXML_Loop_Switch.ps1
@@ -0,0 +1,29 @@
+$XMLFilePath = ""
+$XMLFile = [xml](Get-Content $XMLFilePath)
+$keys = ("key1", "key2", "key3")
+$values = ("value1", "value2", "value3")
+$count=0
+    $Title = "Please select from the choices below:"
+    $Prompt = "Would you like to change the values, check the values or quit?"
+    $Choices = [System.Management.Automation.Host.ChoiceDescription[]] @("&Change Values", "&Values Check", "&Quit")
+    $Default = 1
+
+    $Choice = $host.UI.PromptForChoice($Prompt,$Title,$Choices,$Default)
+    switch($Choice){
+        0 {write-host "Changing values..`n"
+            foreach ($key in $keys) {
+                $targetnode = $XMLFile.SelectSingleNode("//config/Settingsapp/add[@tag='Instructional:Negative'][@value]")
+                $targetnode.SetAttribute("value", $values[$count])
+                write-host "$count. $key value changed to" $values[$count]
+                $count++
+            }
+            $XMLFile.Save($XMLFilePath)
+        }
+        1 {write-host "Checking values...`n"
+            foreach ($key in $keys) {
+                $targetnode = $XMLFile.SelectSingleNode("//config/Settingsapp/add[@tag='Instructional:Negative'][@value]")
+                Write-Output "$($targetnode.OuterXML)"    
+            }
+        }
+        2 {exit}
+    }
